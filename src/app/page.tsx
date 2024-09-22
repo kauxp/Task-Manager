cda6005b9fbb4cd1cfd4d32695fc1a1c947a001e
Paramjeet Kaur <paramjeetkaur4326@gmail.com>
2024-09-23 01:32:01 +0530

    Changed Routing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 import isAuth from "@/components/isAuth";
-
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 function Home() {
-
+  const router = useRouter();
+  useEffect(() => {
+    
+      router.push("/login"); 
+    
+  }, [router]);
   return (
     <div>
       
